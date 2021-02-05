3ad9dc436279ace2782c256bce3145768219f587
Yudai Iwamizu <yu.9824@gmail.com>
2021-02-05 13:39:30 +0900

    Modify requiremnts.txt and some details.

--- setup.py
+++ setup.py
@@ -31,71 +31,49 @@ ICON = os.path.join('gui', 'icon', '{}.icns'.format(APP_NAME))
 PKGS = ['pandas', 'numpy', 'matplotlib', 'element_recognition', 'ternary_diagram']
 
 
-if 'py2app' in sys.argv:
-    alias = '-A' in sys.argv or '--alias' in sys.argv
+# 諸変数・定数の定義
+lib_path = os.path.join(os.environ['CONDA_PREFIX'], 'lib')
+fname_libpython = 'libpython{}.dylib'.format(VERSION_PYTHON)
 
-    # 諸変数・定数の定義
-    lib_path = os.path.join(os.environ['CONDA_PREFIX'], 'lib')
-    fname_libpython = 'libpython{}.dylib'.format(VERSION_PYTHON)
+# libpython3.7.m.dylibだとエラーになるのであらかじめコピーしておく．
+path_original = os.path.join(lib_path, 'libpython{}m.dylib'.format(VERSION_PYTHON))
+path_converted = os.path.join(lib_path, fname_libpython)
+if os.path.exists(path_original) and not os.path.exists(path_converted):
+    copyfile(path_original, path_converted)
 
-    # libpython3.7.m.dylibだとエラーになるのであらかじめコピーしておく．
-    path_original = os.path.join(lib_path, 'libpython{}m.dylib'.format(VERSION_PYTHON))
-    path_converted = os.path.join(lib_path, fname_libpython)
-    if os.path.exists(path_original) and not os.path.exists(path_converted):
-        copyfile(path_original, path_converted)
+# 諸変数の準備
+dylib_files = [os.path.join(lib_path, f) for f in os.listdir(lib_path) if '.dylib' in f]
+contents_path = os.path.join('dist', '{}.app'.format(APP_NAME), 'Contents')
+frameworks_path = os.path.join(contents_path, 'Frameworks')
 
-    # 諸変数の準備
-    dylib_files = [os.path.join(lib_path, f) for f in os.listdir(lib_path) if '.dylib' in f]
-    contents_path = os.path.join('dist', '{}.app'.format(APP_NAME), 'Contents')
-    frameworks_path = os.path.join(contents_path, 'Frameworks')
+OPTIONS = {
+    'argv_emulation': False,
+    'packages': PKGS,
+    'iconfile': ICON,
+    'plist':{
+        'PyRuntimeLocations':[
+            '@executable_path/../Frameworks/{}'.format(fname_libpython),
+            os.path.join(lib_path, fname_libpython),
+        ],
+        'CFBundleName': APP_NAME,
+        'CFBundleDisplayName': APP_NAME,
+        'CFBundleGetInfoString': DESCRIPTION,
+        'CFBundleIdentifier': "com.{0}.osx.{1}".format(ID, APP_NAME),
+        'CFBundleVersion': VERSION,
+        'CFBundleShortVersionString': VERSION,
+        'NSHumanReadableCopyright': u"Copyright © 2020-, {}".format(AUTHOR)
+    },
+    # 'frameworks': dylib_files,
+}
 
-    OPTIONS = {
-        'argv_emulation': False,
-        'packages': PKGS,
-        'iconfile': ICON,
-        'plist':{
-            'PyRuntimeLocations':[
-                '@executable_path/../Frameworks/{}'.format(fname_libpython),
-                os.path.join(lib_path, fname_libpython),
-            ],
-            'CFBundleName': APP_NAME,
-            'CFBundleDisplayName': APP_NAME,
-            'CFBundleGetInfoString': DESCRIPTION,
-            'CFBundleIdentifier': "com.{0}.osx.{1}".format(ID, APP_NAME),
-            'CFBundleVersion': VERSION,
-            'CFBundleShortVersionString': VERSION,
-            'NSHumanReadableCopyright': u"Copyright © 2020-, {}".format(AUTHOR)
-        },
-        # 'frameworks': dylib_files,
-    }
-
-    setup(
-        name = APP_NAME,
-        app = SRC,
-        author = AUTHOR,
-        author_email = EMAIL,
-        version = VERSION,
-        data_files = DATA_FILES,
-        options = {'py2app': OPTIONS},
-        setup_requires = ['py2app'],
-        url = 'https://github.com/{0}/{1}'.format(ID, APP_NAME),
-    )
-
-    # aliasモードじゃないとき．
-    # if not alias:
-    #     {copyfile(f, os.path.join(frameworks_path, os.path.basename(f))) for f in dylib_files}
-else:
-    with open('requirements.txt') as requirements_file:
-        install_requirements = requirements_file.read().splitlines()
-
-    setup(
-        name = APP_NAME,
-        version = VERSION,
-        description = DESCRIPTION,
-        author = AUTHOR,
-        author_email = EMAIL,
-        install_requires = install_requirements,
-        url = 'https://github.com/{0}/{1}'.format(ID, APP_NAME),
-        # license = license,
-        packages = find_packages(exclude=['example'])
-    )
+setup(
+    name = APP_NAME,
+    app = SRC,
+    author = AUTHOR,
+    author_email = EMAIL,
+    version = VERSION,
+    data_files = DATA_FILES,
+    options = {'py2app': OPTIONS},
+    setup_requires = ['py2app'],
+    url = 'https://github.com/{0}/{1}'.format(ID, APP_NAME),
+)
